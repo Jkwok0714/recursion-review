90b8b9eb774e0483671f11c3e088ead9cdb07f3d
Hack Reactor Student <student@hackreactor.com>
2017-08-21 11:26:17 -0700

    Working getElementsByClassName, passes all tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  let arr = [];
+
+  const recurse = function recurseThroughDom(el) {
+    if (el.classList && el.classList.contains(className)) {
+      arr.push(el);
+    }
+    if (el.childNodes) {
+      for (let i = 0; i < el.childNodes.length; i++) {
+        recurseThroughDom(el.childNodes[i]);
+      }
+    }
+
+  }
+
+  recurse(document.body);
+  return arr;
 };
