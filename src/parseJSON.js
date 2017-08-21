2d83faa5e197ac1e2d3f78af4148b1d78dc3ca69
Hack Reactor Student <student@hackreactor.com>
2017-08-21 16:16:19 -0700

    Revert "parse function BAD! revert to last commit"
    This reverts commit 9744b1c1e4301ec0e943bcda66fc65c72cd6f9cd.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -6,18 +6,21 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
 
-  let depth = 1;
+  let depth = 0;
   // your code goes here
   if (json[0] === '[') {
-    if (json[1] === ']') {
-      return [];
-    }
+    if (json[1] === ']') return [];
 
     let container = [];
     let currStr = '';
     let inString = false;
-    for (let i = 1; i < json.length; i++) {
-
+    for (let i = 1; i < json.length-1; i++) {
+      if (i === json.length-2){
+        currStr += json[i];
+        // console.log('cur str is', currStr);
+        container.push(parseJSON(currStr.trim()));
+        currStr = '';
+      }
 
       if (json[i] === '"' && inString) {
         inString = false;
@@ -33,43 +36,46 @@ var parseJSON = function(json) {
         depth--;
       } else if (json[i] === '}') {
         depth--;
-      } else if (json[i] === ',') {
-        if (depth === 1) {
+      } else if (json[i] == ',') {
+        if (depth === 0) {
           container.push(parseJSON(currStr.trim()));
           currStr = '';
           continue;
         }
       }
       currStr += json[i];
-
-      if (i === json.length-1){
-        depth--;
-        //currStr += json[i];
-        // console.log('cur str is', currStr);
-        container.push(parseJSON(currStr.trim()));
-        currStr = '';
-      }
     }
 
-    console.log('Depth is ', depth);
-
-    if (depth !== 0) {
-      //throw new SyntaxError;
+    if (depth === 0 && currStr.length > 0) {
+      // throw new SyntaxError;
       return container;
     } else {
       return container;
     }
   } else if (json[0] === '{') {
-    if (json[1] === '}') {
-      return {};
-    }
+    if (json[1] === '}') return {};
     let container = {};
     let currStr = '';
     let currKey = '';
     let currVal = '';
     let inString = false;
 
-    for (let i = 1; i < json.length; i++) {
+    for (let i = 1; i < json.length-1; i++) {
+      if (depth < 0) {
+        currStr = currStr.slice(0, -1);
+        currVal = currStr;
+        container[parseJSON(currKey.trim())] = parseJSON(currVal.trim());
+        currStr = '';
+        break;
+      }
+      if (i === json.length-2) {
+        currStr += json[i];
+        currVal = currStr;
+        container[parseJSON(currKey.trim())] = parseJSON(currVal.trim());
+        currStr = '';
+        break;
+      }
+
       if (json[i] === '"' && inString) {
         inString = false;
         depth--;
@@ -84,8 +90,8 @@ var parseJSON = function(json) {
         depth--;
       } else if (json[i] === '}') {
         depth--;
-      } else if (json[i] === ',') {
-        if (depth === 1) {
+      } else if (json[i] == ',') {
+        if (depth === 0) {
           currVal = currStr;
           container[parseJSON(currKey.trim())] = parseJSON(currVal.trim());
           currStr = '';
@@ -93,8 +99,8 @@ var parseJSON = function(json) {
           currVal = '';
           continue;
         }
-      } else if (json[i] === ':') {
-        if (depth === 1) {
+      } else if (json[i] == ':') {
+        if (depth === 0) {
           currKey = currStr;
           currStr = '';
           continue;
@@ -103,28 +109,10 @@ var parseJSON = function(json) {
         continue;
       }
       currStr += json[i];
-
-      if (depth === 0) {
-        currStr = currStr.slice(0, -1);
-        currVal = currStr;
-        container[parseJSON(currKey.trim())] = parseJSON(currVal.trim());
-        currStr = '';
-        //depth = 0;
-        break;
-      }
-      if (i === json.length-1) {
-        //currStr += json[i];
-        currVal = currStr;
-        container[parseJSON(currKey.trim())] = parseJSON(currVal.trim());
-        currStr = '';
-        break;
-      }
     }
 
-
-
-    if (depth !== 0) {
-      throw new SyntaxError;
+    if (depth === 0 && currStr.length > 0) {
+      //throw new SyntaxError;
       return container;
     } else {
       return container;
@@ -132,9 +120,7 @@ var parseJSON = function(json) {
 
 
   } else if (json[0] === '"') {
-    if (json[1] === '"') {
-      return '';
-    }
+    if (json[1] === '"') return '';
     let currStr = '';
     for (var i = 1; i<json.length-1; i++) {
       if (json[i] === '\\') {
